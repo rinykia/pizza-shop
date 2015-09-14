cbebf7ff34cde63cd8d129b7dcd9bb20065bb1f7
Natali <rinykia@gmail.com>
2015-09-14 16:14:06 +0300

    play with localStorage variable

--- public/app.js
+++ public/app.js
@@ -1,4 +1,7 @@
-  function something()
-  {
-  	alert('Hello everyone!');
-  }
+function something()
+{
+  var x = window.localStorage.getItem('aaa');
+  window.localStorage.setItem('aaa', 555);
+  alert(x);
+
+}
