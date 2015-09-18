1588d8de66306fe2cff4ac2cbeebf19e747bd3f2
Natali <rinykia@gmail.com>
2015-09-18 17:18:00 +0300

    Make key

--- public/app.js
+++ public/app.js
@@ -8,7 +8,9 @@ function something()
 
 function add_to_cart(id)
 {
-  var x = window.localStorage.getItem('product_' + id);
+  var key = 'product_' + id;
+
+  var x = window.localStorage.getItem(key);
   x = x * 1 + 1;
-  window.localStorage.setItem('product_' + id, x);
+  window.localStorage.setItem(key, x);
 }
